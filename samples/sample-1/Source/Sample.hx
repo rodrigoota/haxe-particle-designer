a2f1114bd90e77983deacfb94cd5eab9f6b0fcea
mourlamstudios@gmail.com <mourlamstudios@gmail.com@c6a3b672-fd20-ecf4-9116-78679dbec569>
2012-04-15 17:45:42 +0000

    First commit. Few issues we noticed (no blend mode). We need to get this fixed. Particle systems NEED an ADD blend mode more often than not. Gravity (or possibly forces) seem to be inverted compared to particle designer? Should be a simple fix. Radial systems not working. You will see trace log for "Mode B not supported yet". We'll get this up in our next update!

---
+++ samples/sample-1/Source/Sample.hx
@@ -0,0 +1,66 @@
+package;
+
+
+import nme.display.Sprite;
+import nme.display.StageAlign;
+import nme.display.StageQuality;
+import nme.display.StageScaleMode;
+import nme.events.Event;
+import nme.Lib;
+import nme.events.MouseEvent;
+import nme.Assets;
+import nme.ui.Mouse;
+
+import nme.display.Bitmap;
+import nme.display.BitmapData;
+
+class Sample extends Sprite
+{
+	
+
+	public function new()
+	{
+		super ();
+
+		init();
+	}
+
+	private function init()
+	{
+		Lib.current.stage.align = StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		Lib.current.stage.addEventListener (Event.ACTIVATE, stage_onActivate);
+		Lib.current.stage.addEventListener (Event.DEACTIVATE, stage_onDeactivate);
+
+		//Create a particle system
+		//Pass the file name of the plist and the path for both the plist and the particle textures (.pngs)
+		/**
+		Be sure to put in trailing "/"" for assets path (2nd argument)
+		**/
+		var			particleSystem:ASParticleSystem;
+		particleSystem = ASParticleSystem.particleWithFile("pinkStream.plist","assets/particles/");
+		addChild(particleSystem);
+	}
+	
+	
+	// Event Handlers
+	
+	private function stage_onActivate (event:Event):Void
+	{
+		trace("stage_onActivate");
+	}
+	
+	
+	private function stage_onDeactivate (event:Event):Void {
+		trace("stage_onDeactivate");
+		
+	}
+
+	public static function main() {
+
+		//Redirect trace logs from the fucking screen
+		//org.flashdevelop.utils.FlashConnect.redirect();
+
+		Lib.current.addChild(new Sample());
+	}
+}
